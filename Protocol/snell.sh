15b66111031dbb1d7a35272ea4525d26e353d4f8
MUTU <96739481+emberdusk@users.noreply.github.com>
2024-11-07 16:44:08 +0800

    Modified by EmberDusk

--- Protocol/snell.sh
+++ Protocol/snell.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH1
+export PATH
 
 #=================================================
 #	System Required: CentOS/Debian/Ubuntu
